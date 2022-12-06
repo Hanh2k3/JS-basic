c67257fe1774e65b2908c44f12e385874680cbd4
Hanh <hanhlv.21it@vku.udn.vn>
2022-12-06 11:00:30 +0700

    if statement

---
+++ lab4-string/main.js
@@ -0,0 +1,25 @@
+// let fullName = "Le Van hanh"; 
+
+// let firstName = fullName.slice(fullName.lastIndexOf(" ") + 1);
+// let lastName  = fullName.slice(0, fullName.lastIndexOf(" "));
+
+// console.log(firstName.charAt(0).toUpperCase() + firstName.slice(1).toLowerCase());
+// console.log(fullName.replaceAll(" ", "*"));
+
+document.getElementById("btnSubmit").onclick = function() {
+    let cardMaster = document.getElementById("cardMaster");
+    let cardVisa = document.getElementById("cardVisa");
+    let cardPaypal = document.getElementById("cardPaypal");
+
+   if(cardMaster.checked) {
+        console.log('card master'); 
+   } else if(cardVisa.checked) {
+
+   } else if(cardPaypal.checked) {
+        console.log('card paypal');
+   } else {
+        console.log('no choose'); 
+   }
+   
+
+}
